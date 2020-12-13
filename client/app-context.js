5c90e6c860d5c477e2db9fdde5db26b41065b1fb
Tim Davis <contact@timdav.is>
2020-12-13 16:49:52 +0000

    Give default values to AppContext.

--- client/app-context.js
+++ client/app-context.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const AppContext = React.createContext(null);
+const AppContext = React.createContext({
+  user: null,
+  view: {
+    path: '',
+    params: new URLSearchParams()
+  },
+  handleSignIn: ({ user, token }) => {},
+  handleSignOut: () => {}
+});
 
 export default AppContext;
